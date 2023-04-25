dd785046e65b8bfa7e45ab824b6703e1a70259ca
Akshay Rathod <kr.akshay234@gmail.com>
2023-04-25 23:28:16 +0530

    updated

--- QB/ClassesAndObjects/integerToRomanNumeral.py
+++ QB/ClassesAndObjects/integerToRomanNumeral.py
@@ -1,22 +1,16 @@
-class py_solution:
-    def int_to_Roman(self, num):
-        val = [
-        1000, 900, 500, 400,
-        100, 90, 50, 40,
-        10, 9, 5, 4, 1]
-        syb = [
-        "M", "CM", "D", "CD",
-        "C", "XC", "L", "XL",
-        "X", "IX", "V", "IV",
-        "I"
-        ]
+class Roman:
+    def intToRoman(self, num):
+        val = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        roman = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
+
         roman_num = ''
         i = 0
         while num > 0:
+
             for _ in range(num // val[i]):
-                roman_num += syb[i]
+                roman_num += roman[i]
                 num -= val[i]
-                i += 1
+            i+= 1
         return roman_num
-print(py_solution().int_to_Roman(1))
-print(py_solution().int_to_Roman(4000))
+
+print(Roman().intToRoman(4001))
