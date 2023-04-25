1c466eed5efba4556faaaf7f87207080ddb95a6f
Akshay Rathod <kr.akshay234@gmail.com>
2023-04-25 23:15:43 +0530

    updated

--- QB/Decimal/deep_copy.py
+++ QB/Decimal/deep_copy.py
@@ -4,6 +4,11 @@ print("Original list: ", nums_x)
 nums_y = copy.deepcopy(nums_x)
 print("\nDeep copy of the said list:")
 print(nums_y)
+print("\nChange the value of an element of the original list:")
+nums_x[1][1] = 10
+print(nums_x)
+print("\nDeep copy of the said list:")
+print(nums_y)
 
 nums_x = {"a":1, "b":2, 'cc':{"c":3}}
 print("Original dictionary: ", nums_x)
