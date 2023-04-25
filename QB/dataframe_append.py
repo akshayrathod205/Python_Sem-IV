157f78caf2a35516bf3006cfb22bb1b56f93d2a3
Akshay Rathod <kr.akshay234@gmail.com>
2023-04-25 16:30:46 +0530

    uploaded files

---
+++ QB/dataframe_append.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+exam_data = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Trevor', 'Franklin', 'Patrick'],
+'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+df = pd.DataFrame(exam_data, index=labels)
+print("Original:")
+print(df)
+
+df.loc['k'] = ['Suresh', 15.5, 1, 'yes']
+print("New Dataframe:")
+print(df)
+
+df = df.drop('k')
+print("Data frame after deleting row 'k'")
+print(df)
